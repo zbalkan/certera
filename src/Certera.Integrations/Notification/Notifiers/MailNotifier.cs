ef63df7a23e6d10cb2a49af4adae480c9282af56
Zafer Balkan <zafer@zaferbalkan.com>
2022-12-24 00:15:29 +0200

    Implemented MailNotifier

--- src/Certera.Integrations/Notification/Notifiers/MailNotifier.cs
+++ src/Certera.Integrations/Notification/Notifiers/MailNotifier.cs
@@ -1,12 +1,82 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Linq;
+using MailKit.Net.Smtp;
+using MimeKit;
 
 namespace Certera.Integrations.Notification.Notifiers
 {
-
     public class MailNotifier : INotifier
     {
-        public async Task TrySendAsync(string body, List<string> recipients, string subject = null) => throw new NotImplementedException();
+        private readonly SmtpClient _client;
+        private readonly MailSenderInfo _info;
+
+        public MailNotifier(MailSenderInfo info)
+        {
+            _client = new SmtpClient();
+            _info = info;
+        }
+
+        public async Task TrySendAsync(string body, List<string> recipients, string subject = null)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress(_info.FromName, _info.FromEmail));
+            message.To.AddRange(recipients.Select(x => MailboxAddress.Parse(x)));
+            message.Subject = subject;
+            message.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = body
+            };
+
+            await EnsureConnectedAsync();
+
+            await _client.SendAsync(message);
+        }
+
+        private async Task EnsureConnectedAsync()
+        {
+            if (!_client.IsConnected)
+            {
+                await _client.ConnectAsync(_info.Host, _info.Port, _info.UseSsl).ConfigureAwait(false);
+
+                if (_info.Username != null || _info.Password != null)
+                {
+                    await _client.AuthenticateAsync(_info.Username, _info.Password).ConfigureAwait(false);
+                }
+            }
+        }
+
+        #region IDisposable Support
+
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _client?.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose() => Dispose(true);
+
+        #endregion IDisposable Support
+    }
+
+    // TODO: Use options pattern for settings
+    public class MailSenderInfo
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public bool UseSsl { get; set; }
+        public string FromEmail { get; set; }
+        public string FromName { get; set; }
     }
 }
